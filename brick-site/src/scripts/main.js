256742f3f68ef6132c96feac9e67daf63ec76a71
parker <parkerasnicar@gmail.com>
2025-07-24 20:40:20 +1000

    Menu Button Expansion
    - Added menu expansion

--- brick-site/src/scripts/main.js
+++ brick-site/src/scripts/main.js
@@ -13,3 +13,12 @@ window.addEventListener("DOMContentLoaded", () => {
     inner.appendChild(clone);
   }
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const menuBtn = document.querySelector('button');
+  const menuPanel = document.getElementById('menuPanel');
+
+  menuBtn.addEventListener('click', () => {
+    menuPanel.classList.toggle('open');
+  });
+});
