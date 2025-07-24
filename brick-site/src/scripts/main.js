4a887279fad9ffeb85d21d3094d64235f5967e9a
parker <parkerasnicar@gmail.com>
2025-07-24 20:27:50 +1000

    Revolving Text And More
    - Added Revolving Text
    - Removed Useless Paragraph
    - Added 3rd Font

--- brick-site/src/scripts/main.js
+++ brick-site/src/scripts/main.js
@@ -1,1 +1,15 @@
-// This file is intentionally left blank.
+window.addEventListener("DOMContentLoaded", () => {
+  const container = document.querySelector(".revolving-text");
+  const inner = document.querySelector(".revolving-inner");
+  const span = inner.querySelector("span");
+
+  const containerWidth = container.offsetWidth;
+  const spanWidth = span.offsetWidth;
+
+  const repeatCount = Math.ceil((containerWidth * 2) / spanWidth);
+
+  for (let i = 1; i < repeatCount; i++) {
+    const clone = span.cloneNode(true);
+    inner.appendChild(clone);
+  }
+});
